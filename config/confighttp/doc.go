f341e6f45ea30363b8acd84f41822032620818e5
Jason Polanco <polajaso@amazon.com>
2023-07-07 18:41:10 -0400

    Add otel confighttp to branch (#43)

---
+++ config/confighttp/doc.go
@@ -0,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package confighttp defines the configuration settings
+// for creating an HTTP client and server.
+package confighttp // import "github.com/amazon-contributing/opentelemetry-collector-contrib/config/confighttp"
